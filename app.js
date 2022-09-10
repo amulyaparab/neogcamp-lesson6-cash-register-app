617acfa6d4ac29a1cc08eead66ce52a6757654ad
amulya-parab <amulyaparab101@gmail.com>
2022-09-10 17:05:35 +0530

    updated app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnPrimary = document.querySelector("#btn-primary");
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+
+btnPrimary.addEventListener("click", clickHandler);
+function clickHandler() {
+  console.log("clicked");
+}
