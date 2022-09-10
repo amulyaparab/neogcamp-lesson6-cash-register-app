5ef323c2bdfdc62498b4882bb2aa1f4aad729acd
amulya-parab <amulyaparab101@gmail.com>
2022-09-10 18:34:28 +0530

    added functions and testing

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var btnPrimary = document.querySelector("#btn-primary");
 var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-given");
+var message = document.querySelector("#error-message");
 
-btnPrimary.addEventListener("click", clickHandler);
-function clickHandler() {
-  console.log("clicked");
-}
+var availableNotes = ["2000", "500", "200", "100", "50", "20", "10", "1"];
+
+btnPrimary.addEventListener("click", function validateBillAmount() {
+  if (billAmount > 0) {
+    console.log("ok");
+    // if (cashGiven > billAmount) {
+    // } else {
+    //   console.log("Do you want to wash dishes and ?");
+    // }
+  } else {
+    console.log("Do you want to wash dishes?");
+  }
+});
+
+// function showMessage(msg) {
+//   message.style.display = "block";
+// }
