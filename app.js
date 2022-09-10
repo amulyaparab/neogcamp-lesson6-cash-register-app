a4fe186df162bcd726ee7326d614bdf19ffac9c4
amulya-parab <amulyaparab101@gmail.com>
2022-09-10 19:10:35 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -6,17 +6,25 @@ var message = document.querySelector("#error-message");
 var availableNotes = ["2000", "500", "200", "100", "50", "20", "10", "1"];
 
 btnPrimary.addEventListener("click", function validateBillAmount() {
-  if (billAmount > 0) {
-    console.log("ok");
-    // if (cashGiven > billAmount) {
-    // } else {
-    //   console.log("Do you want to wash dishes and ?");
-    // }
+  message.style.display = "none";
+
+  if (billAmount.value > 0) {
+    if (cashGiven.value > billAmount.value) {
+      hideMessage();
+      console.log("ok");
+    } else {
+      showMessage();
+      message.innerText = "Do you want to wash dishes ?";
+    }
   } else {
-    console.log("Do you want to wash dishes?");
+    showMessage();
+    message.innerText = "Invalid Bill Amount";
   }
 });
 
-// function showMessage(msg) {
-//   message.style.display = "block";
-// }
+function hideMessage(msg) {
+  message.style.display = "none";
+}
+function showMessage(msg) {
+  message.style.display = "block";
+}
