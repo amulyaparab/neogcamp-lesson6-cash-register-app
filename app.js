b62c30daf6bfe695fc5c97ad77aebbd8aeb7a58e
amulya-parab <amulyaparab101@gmail.com>
2022-09-19 18:30:53 +0530

    docs: added conditions

--- app.js
+++ app.js
@@ -3,24 +3,49 @@ var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-given");
 var message = document.querySelector("#error-message");
 var noOfNotes = document.querySelectorAll(".no-of-notes");
+var table = document.querySelector("#table");
 var availableNotes = ["2000", "500", "200", "100", "50", "20", "10", "1"];
 
-btnPrimary.addEventListener("click", function validateBillAmount() {
-  if (billAmount.value >= 0) {
-    if (cashGiven.value >= billAmount.value) {
-      hideMessage();
-      var amountToBeReturned = cashGiven.value - billAmount.value;
+btnPrimary.addEventListener("click", () => {
+  var newBillAmount = Number(billAmount.value);
+  var newCashGiven = Number(cashGiven.value);
+  hideMessage();
+  if (newBillAmount > 0) {
+    if (newCashGiven >= newBillAmount) {
+      showTable(table);
+      var amountToBeReturned = newCashGiven - newBillAmount;
       calculateChange(amountToBeReturned);
+      showMessage("Thanks! Come back again😊");
+    } else if (
+      newBillAmount > newCashGiven &&
+      newCashGiven != "" &&
+      newCashGiven > 0
+    ) {
+      showMessage("Do you want to wash plates ?🧼🍽️");
+      hideTable(table);
+    } else if (newCashGiven < 0) {
+      hideMessage();
+      hideTable(table);
+      showMessage(" I need Actual Cash Man😒");
     } else {
-      showMessage("Do you want to wash dishes ?");
+      hideMessage();
+      hideTable(table);
+      showMessage("Gimme Cash 🖐️💸");
     }
   } else {
-    showMessage("Invalid Bill Amount");
+    showMessage("Invalid Bill Amount❌");
+    hideTable(table);
   }
 });
 
-function hideMessage(msg) {
+function hideMessage() {
   message.style.display = "none";
+}
+function hideTable(table) {
+  table.style.display = "none";
+}
+function showTable(table) {
+  table.style.display = "block";
 }
 function showMessage(msg) {
   message.style.display = "block";
